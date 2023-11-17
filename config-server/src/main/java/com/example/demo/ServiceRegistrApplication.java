c9c5add88dbcae08ae4906efdeaeae3e3826e557
Atharva Wader <awader28@gmail.com>
2023-11-17 20:25:37 +0530

    First Commit Contains working service registry and user service

---
+++ config-server/src/main/java/com/example/demo/ServiceRegistrApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrApplication.class, args);
+	}
+
+}
